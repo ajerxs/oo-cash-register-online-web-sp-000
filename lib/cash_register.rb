b240580ed59fe63241f8524e6d2bf2b153d611d8
Andrew Eames <ajerxs@gmail.com>
2020-11-29 00:14:23 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,18 @@ class CashRegister
   end
 
   def add_item(title, price, quantity = 1)
-
+    if quantity > 1
+      i = 0
+      while i < quantity
+        @items << title
+        i += 1 
+      end
+    else
+      @items << title
+    end
+    @total += price * quantity
+    @amount = @total
+    @total
   end
 
 end
