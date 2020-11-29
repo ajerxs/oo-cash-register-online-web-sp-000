adb63b8e53def646a49f30824f673c8ddf826b11
Andrew Eames <ajerxs@gmail.com>
2020-11-29 00:22:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -27,6 +27,9 @@ class CashRegister
       @discount = @discount/100.to_f
       @total = (1 - @discount) * @total
       "After the discount, the total comes to $#{@total.to_i}."
+    else
+      "There is no disount to apply."
     end
   end
+  
 end
