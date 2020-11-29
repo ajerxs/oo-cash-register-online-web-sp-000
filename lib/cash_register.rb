fb2644e696191a4041d55377ca222967147409d3
Andrew Eames <ajerxs@gmail.com>
2020-11-29 00:08:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,11 @@
+class CashRegister
+  attr_accessor :total, :discount, :amount, :items
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = items
+  end
+
+  
+end
